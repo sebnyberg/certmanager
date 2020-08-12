fc29852e4e599078a474ab0a669b9760e8704558
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2020-08-12 20:36:28 +0200

    create MTLS functions

--- tls.go
+++ tls.go
@@ -9,13 +9,24 @@ import (
 	"github.com/square/certstrap/pkix"
 )
 
-func GetTLSConfig(
+type signedCertificate struct {
+	caCert      *pkix.Certificate
+	caCertBytes []byte
+	cert        *pkix.Certificate
+	certBytes   []byte
+	certPool    *x509.CertPool
+	tlsCert     tls.Certificate
+	key         *pkix.Key
+	keyBytes    []byte
+}
+
+func getTLSConfig(
 	ctx context.Context,
 	caURL string,
 	caPassword string,
 	commonName string,
 	sans []string,
-) (*tls.Config, error) {
+) (*signedCertificate, error) {
 	ca509Cert, caRSAKey, err := GetCert(ctx, caURL, caPassword)
 	if err != nil {
 		return nil, err
@@ -53,9 +64,57 @@ func GetTLSConfig(
 		return nil, err
 	}
 
+	signedCert := signedCertificate{
+		caCert:      caCert,
+		caCertBytes: caCertBytes,
+		cert:        cert,
+		certBytes:   certBytes,
+		certPool:    certPool,
+		tlsCert:     tlsCert,
+		key:         key,
+		keyBytes:    keyBytes,
+	}
+
+	return &signedCert, nil
+}
+
+func MTLSClientConfig(
+	ctx context.Context,
+	caURL string,
+	caPassword string,
+	clientName string,
+	serverName string,
+) (*tls.Config, error) {
+	signedCert, err := getTLSConfig(ctx, caURL, caPassword, clientName, nil)
+	if err != nil {
+		return nil, err
+	}
+
 	tlsConf := tls.Config{
-		Certificates: []tls.Certificate{tlsCert},
-		RootCAs:      certPool,
+		Certificates: []tls.Certificate{signedCert.tlsCert},
+		RootCAs:      signedCert.certPool,
+		ServerName:   serverName,
+	}
+
+	return &tlsConf, nil
+}
+
+func MTLSServerConfig(
+	ctx context.Context,
+	caURL string,
+	caPassword string,
+	hostname string,
+	altNames []string,
+) (*tls.Config, error) {
+	signedCert, err := getTLSConfig(ctx, caURL, caPassword, hostname, altNames)
+	if err != nil {
+		return nil, err
+	}
+
+	tlsConf := tls.Config{
+		Certificates: []tls.Certificate{signedCert.tlsCert},
+		ClientCAs:    signedCert.certPool,
+		ClientAuth:   tls.RequireAndVerifyClientCert,
 	}
 
 	return &tlsConf, nil
